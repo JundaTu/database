28f63e17fa9516927dd3a576825d3b06041685b8
JundaTu <jundatu2017@u.northwestern.edu>
2018-11-12 15:34:24 -0600

    add a util class to project

---
+++ src/com/jdbcHelper/DBHelper.java
@@ -0,0 +1,11 @@
+package com.jdbcHelper;
+
+/**
+ * @program database
+ * @description: TODO: connect to mysql and close connection
+ * @author: tujunda
+ * @create: 2018/11/12 15:33
+ */
+public class DBHelper {
+}
+
