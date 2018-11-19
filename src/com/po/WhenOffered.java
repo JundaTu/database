08cd5461a1e19fdb2d32a1a409050a0e6cc2080d
JundaTu <jundatu2017@u.northwestern.edu>
2018-11-18 21:27:59 -0600

    add the rest POs

---
+++ src/com/po/WhenOffered.java
@@ -0,0 +1,37 @@
+package com.po;
+
+/**
+ * @program database
+ * @description: TODO:
+ * @author: tujunda
+ * @create: 2018/11/18 21:25
+ */
+public class WhenOffered {
+    private String UoSCode;
+    private String semester;
+
+    public String getUoSCode() {
+        return UoSCode;
+    }
+
+    public void setUoSCode(String uoSCode) {
+        UoSCode = uoSCode;
+    }
+
+    public String getSemester() {
+        return semester;
+    }
+
+    public void setSemester(String semester) {
+        this.semester = semester;
+    }
+
+    @Override
+    public String toString() {
+        return "WhenOffered{" +
+                "UoSCode='" + UoSCode + '\'' +
+                ", semester='" + semester + '\'' +
+                '}';
+    }
+}
+
